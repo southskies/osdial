f2be66d8f72f62dccc7c86ae757d05195456c4a1
Lott Caskey <lottcaskey@gmail.com>
2009-03-15 18:48:05 +0000

    Update trunk to RB-2.1.0b1

---
+++ extras/sql/204003.sql
@@ -0,0 +1,8 @@
+#Create some custom indexes.
+create index country_postal ON osdial_postal_codes (country_code,postal_code);
+create index country_area ON osdial_phone_codes (country_code,areacode);
+create index country_state ON osdial_phone_codes (country_code,state);
+create index country_code ON osdial_phone_codes (country_code);
+create index phone_list ON osdial_list (phone_number,list_id);
+
+UPDATE system_settings SET version='2.0.4.003';
---
+++ extras/sql/210001.sql
@@ -0,0 +1,2 @@
+# 03/13/2009
+UPDATE system_settings SET version='2.1.0.001';
