1ab3fa7f5c8fd59af2369743dda663a1dc3978da
Lott Caskey <lottcaskey@gmail.com>
2009-03-30 19:12:56 +0000

    Add copyright header.

--- extras/sql/upgrade_sql.pl
+++ extras/sql/upgrade_sql.pl
@@ -1,5 +1,28 @@
 #!/usr/bin/perl
-
+#
+# upgrade_sql.pl:  A script to provide easy database migrations from one version
+#                  to anohter.
+#
+# Copyright (C) 2009  Lott Caskey  <lottcaskey@gmail.com>    LICENSE: AGPLv3
+#
+##     This file is part of OSDial.
+##
+##     OSDial is free software: you can redistribute it and/or modify
+##     it under the terms of the GNU Affero General Public License as
+##     published by the Free Software Foundation, either version 3 of
+##     the License, or (at your option) any later version.
+##
+##     OSDial is distributed in the hope that it will be useful,
+##     but WITHOUT ANY WARRANTY; without even the implied warranty of
+##     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##     GNU Affero General Public License for more details.
+##
+##     You should have received a copy of the GNU Affero General Public
+##     License along with OSDial.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+# Todo:
+# upstream vendor converion
 
 use strict;
 use DBI;
