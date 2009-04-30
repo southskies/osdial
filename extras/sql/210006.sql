d2b21e169d2a474fae8fabebc5884ad47d271f6e
Lott Caskey <lottcaskey@gmail.com>
2009-04-30 19:55:51 +0000

    Fix missing ; in 210006.sql

--- extras/sql/210006.sql
+++ extras/sql/210006.sql
@@ -1,4 +1,4 @@
 # 04/30/2009
 UPDATE system_settings SET version='2.1.0.006';
 
-ALTER TABLE osdial_campaigns ADD campaign_call_time VARCHAR(10) DEFAULT '',
+ALTER TABLE osdial_campaigns ADD campaign_call_time VARCHAR(10) DEFAULT '';
