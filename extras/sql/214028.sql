d5cb96f5087b36618bc17a0158b6c03686b766e9
Lott Caskey <lottcaskey@gmail.com>
2009-07-09 18:45:51 +0000

    Fix for live_agents with not enough closer_campaign space.

--- extras/sql/214028.sql
+++ extras/sql/214028.sql
@@ -9,5 +9,7 @@ CREATE INDEX uniqueid ON osdial_agent_log (uniqueid);
 ALTER TABLE recording_log ADD uniqueid VARCHAR(20) NOT NULL default '';
 CREATE INDEX uniqueid ON recording_log (uniqueid);
 
+ALTER TABLE osdial_live_agents MODIFY closer_campaigns TEXT default '';
+
 UPDATE system_settings SET version='2.1.4.028';
 UPDATE system_settings SET last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);
