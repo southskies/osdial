7f6106dd5f8b6307ba53b335982f4954e48c967c
Lott Caskey <lottcaskey@gmail.com>
2009-08-16 19:18:15 +0000

    Added osdial_lists.cost for default lead cost.  Added osdial_list.cost for actual lead cost.  osdial_users.manual_dial_new_limit for setting a limit on the number of NEW leads an agent can be given.  Added osdial_agent_log.lead_called_count to retain the count of the lead at the time the agent touched it.  Added osdial_agent_log.prev_status to record the status of the lead prior to the agent dispositioning it.  Added table osdial_campaign_agent_stats which is populated through the AST_VDcampaign_stats.pl process and is identical in functionality to osdial_campaign_stats or osdial_campaign_server_stats, only recording stats on a campaign/agent basis.  Update 999999.sql for added cost field on default records.

---
+++ extras/sql/215031.sql
@@ -0,0 +1,54 @@
+# 07/13/2009
+
+ALTER TABLE osdial_lists ADD cost FLOAT default '0.00';##|##
+ ##Sets the default cost to be assigned to each loaded lead.;
+
+ALTER TABLE osdial_list ADD cost FLOAT default '0.00';##|##
+ ##Allow for a cost to be assigned to each lead.;
+
+ALTER TABLE osdial_users ADD manual_dial_new_limit INT(9) default '0';##|##
+ ##Adds a limit on the number of new leads an agent can take in a day.
+ ##    Only applies to MANUAL dials.;
+
+ALTER TABLE osdial_agent_log ADD lead_called_count INT(9) default '0';##|##
+ ##Add lead_called_count to agent_log.
+
+ALTER TABLE osdial_agent_log ADD prev_status VARCHAR(6) default '';##|##
+ ##Add prev_status to agent_log.
+
+CREATE TABLE osdial_campaign_agent_stats (
+  campaign_id varchar(20) NOT NULL,
+  user varchar(20) default '',
+  update_time timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  manual_dial_new_today int(9) unsigned default '0',
+  calls_today int(9) unsigned default '0',
+  answers_today int(9) unsigned default '0',
+  calls_hour int(9) unsigned default '0',
+  answers_hour int(9) unsigned default '0',
+  calls_halfhour int(9) unsigned default '0',
+  answers_halfhour int(9) unsigned default '0',
+  calls_fivemin int(9) unsigned default '0',
+  answers_fivemin int(9) unsigned default '0',
+  calls_onemin int(9) unsigned default '0',
+  answers_onemin int(9) unsigned default '0',
+  status_category_1 varchar(20) default NULL,
+  status_category_count_1 int(9) unsigned default '0',
+  status_category_2 varchar(20) default NULL,
+  status_category_count_2 int(9) unsigned default '0',
+  status_category_3 varchar(20) default NULL,
+  status_category_count_3 int(9) unsigned default '0',
+  status_category_4 varchar(20) default NULL,
+  status_category_count_4 int(9) unsigned default '0',
+  status_category_hour_count_1 int(9) unsigned default '0',
+  status_category_hour_count_2 int(9) unsigned default '0',
+  status_category_hour_count_3 int(9) unsigned default '0',
+  status_category_hour_count_4 int(9) unsigned default '0',
+  PRIMARY KEY  (campaign_id,user)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;##|##
+  ##Adds a table for tracking agent stats per campaign.
+
+
+UPDATE system_settings SET version='2.1.5.031';##|##
+ ##Updating database to version 2.1.5.031;
+UPDATE system_settings SET last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
+ ##Clearing last_update_check flag.;
