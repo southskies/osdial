2a08f6877c674fdcbc8b0a0b45a09e640cee8f9c
Lott Caskey <lottcaskey@gmail.com>
2009-08-19 08:19:06 +0000

    Missing closing tags on SQL description.

--- extras/sql/215031.sql
+++ extras/sql/215031.sql
@@ -11,10 +11,10 @@ ALTER TABLE osdial_users ADD manual_dial_new_limit INT(9) default '0';##|##
  ##    Only applies to MANUAL dials.;
 
 ALTER TABLE osdial_agent_log ADD lead_called_count INT(9) default '0';##|##
- ##Add lead_called_count to agent_log.
+ ##Add lead_called_count to agent_log.;
 
 ALTER TABLE osdial_agent_log ADD prev_status VARCHAR(6) default '';##|##
- ##Add prev_status to agent_log.
+ ##Add prev_status to agent_log.;
 
 CREATE TABLE osdial_campaign_agent_stats (
   campaign_id varchar(20) NOT NULL,
@@ -45,19 +45,19 @@ CREATE TABLE osdial_campaign_agent_stats (
   status_category_hour_count_4 int(9) unsigned default '0',
   PRIMARY KEY  (campaign_id,user)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;##|##
-  ##Adds a table for tracking agent stats per campaign.
+  ##Adds a table for tracking agent stats per campaign.;
 
 CREATE INDEX entry_date ON osdial_list (entry_date);##|##
-  ## Adds entry_date to osdial_list as an index to sort on
+  ## Adds entry_date to osdial_list as an index to sort on;
 
 CREATE INDEX modify_date ON osdial_list (modify_date);##|##
-  ## Adds modify_date to osdial_list as an index to sort on
+  ## Adds modify_date to osdial_list as an index to sort on;
 
 CREATE INDEX area_code ON osdial_list (phone_number(3));##|##
-  ## Adds area_code to osdial_list as an index to sort on
+  ## Adds area_code to osdial_list as an index to sort on;
 
 CREATE INDEX last_name ON osdial_list (last_name(3));##|##
-  ## Adds last_name to osdial_list as an index to sort on
+  ## Adds last_name to osdial_list as an index to sort on;
 
 
 UPDATE system_settings SET version='2.1.5.031';##|##
