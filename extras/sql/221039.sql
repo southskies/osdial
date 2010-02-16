439b7ecc3e7043330f5bb7c87f0170fcf068044e
Lott Caskey <lottcaskey@gmail.com>
2010-02-16 23:44:00 +0000

    AMI host: replace localhost with dotted notation 127.0.0.1 in servers table.

--- extras/sql/221039.sql
+++ extras/sql/221039.sql
@@ -6,5 +6,8 @@ ALTER TABLE osdial_lists ADD web_form_address VARCHAR(255) default '';##|##
 ALTER TABLE osdial_lists ADD web_form_address2 VARCHAR(255) default '';##|##
  ##    Add web_form_address2 to lists.;
 
+UPDATE servers SET telnet_host='127.0.0.1' WHERE telnet_host='localhost';##|##
+ ##    AMI host: replace localhost with dotted notation in servers table.;
+
 UPDATE system_settings SET version='2.2.1.039',last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
  ##    Updating database to version 2.2.1.039 and clearing last_update_check flag.;
