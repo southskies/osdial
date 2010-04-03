4b8397ae74de1018056d6c7156c5d30e498fb21c
Lott Caskey <lottcaskey@gmail.com>
2010-04-03 23:59:12 +0000

    Added a sql_time field to server_updaters so time-sync problems can be quickly determined.

---
+++ extras/sql/221053.sql
@@ -0,0 +1,13 @@
+# 04/03/2010
+
+ALTER TABLE server_updater ADD sql_time timestamp NOT NULL default CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;##|##
+ ##    Increase filename size in recording_log.;
+
+DELETE FROM server_updater;##|##
+ ##    Clear server updater records.
+
+ALTER TABLE server_updater ADD PRIMARY KEY (server_ip);##|##
+ ##    Add primary key.;
+
+UPDATE system_settings SET version='2.2.1.053',last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
+ ##    Updating database to version 2.2.1.053 and clearing last_update_check flag.;
