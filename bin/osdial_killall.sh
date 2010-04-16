b59c79c1e51637e36a02f524347fd9b9eeb049ed
Lott Caskey <lottcaskey@gmail.com>
2010-04-16 13:32:40 +0000

    Add OSD to killall.

--- bin/osdial_killall.sh
+++ bin/osdial_killall.sh
@@ -2,3 +2,4 @@
 
 [ -n "`ps -ef | grep FastAGI`" ] && kill -9 `ps -ef | grep FastAGI | awk '{ print $2 }'` > /dev/null 2>&1
 [ -n "`ps -ef | grep AST`" ] && kill -9 `ps -ef | grep AST | awk '{ print $2 }'` > /dev/null 2>&1
+[ -n "`ps -ef | grep OSD`" ] && kill -9 `ps -ef | grep AST | awk '{ print $2 }'` > /dev/null 2>&1
