e13cbc7a94522aa4c449de35f2771afb46f34e68
Lott Caskey <lottcaskey@gmail.com>
2010-05-01 01:30:13 +0000

    Increase size of web_form_address and web_form_address2 in campaigns, lists, in-groups to 2000 characters.

--- extras/sql/221055.sql
+++ extras/sql/221055.sql
@@ -3,5 +3,23 @@
 ALTER TABLE osdial_hopper MODIFY status enum('READY','QUEUE','INCALL','DONE','HOLD','API') DEFAULT 'READY';##|##
  ##    Added status for API insertion.;
 
+ALTER TABLE osdial_campaigns MODIFY web_form_address VARCHAR(2000) DEFAULT '/osdial/agent/webform_redirect.php';##|##
+ ##    Set max length on campaign web-form1 to 2000 characters.;
+
+ALTER TABLE osdial_campaigns MODIFY web_form_address2 VARCHAR(2000) DEFAULT '/osdial/agent/webform_redirect.php';##|##
+ ##    Set max length on campaign web-form2 to 2000 characters.;
+
+ALTER TABLE osdial_lists MODIFY web_form_address VARCHAR(2000) DEFAULT '/osdial/agent/webform_redirect.php';##|##
+ ##    Set max length on lists web-form1 to 2000 characters.;
+
+ALTER TABLE osdial_lists MODIFY web_form_address2 VARCHAR(2000) DEFAULT '/osdial/agent/webform_redirect.php';##|##
+ ##    Set max length on lists web-form2 to 2000 characters.;
+
+ALTER TABLE osdial_inbound_groups MODIFY web_form_address VARCHAR(2000) DEFAULT '/osdial/agent/webform_redirect.php';##|##
+ ##    Set max length on in-groups web-form1 to 2000 characters.;
+
+ALTER TABLE osdial_inbound_groups MODIFY web_form_address2 VARCHAR(2000) DEFAULT '/osdial/agent/webform_redirect.php';##|##
+ ##    Set max length on in-groups web-form2 to 2000 characters.;
+
 UPDATE system_settings SET version='2.2.1.055',last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
  ##    Updating database to version 2.2.1.055 and clearing last_update_check flag.;
