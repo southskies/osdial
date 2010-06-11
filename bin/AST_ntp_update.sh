b538eea440a5164649aa891f76d6318c5a52e5f6
Lott Caskey <lottcaskey@gmail.com>
2010-06-11 03:31:05 +0000

    Update ntp script to try priviledged port upon failure.

--- bin/AST_ntp_update.sh
+++ bin/AST_ntp_update.sh
@@ -24,22 +24,32 @@ SERVER2="ntp-s1.cise.ufl.edu"
 SERVER3="navobs1.gatech.edu"
 SERVER4="tick.usno.navy.mil"
 SERVER5="time-nw.nist.gov"
-OPTS="-s -u"
 
-/usr/sbin/ntpdate $OPTS $SERVER1
-RETVAL=$?
-if [ $RETVAL -ne 0 ]; then
-	/usr/sbin/ntpdate $OPTS $SERVER2
+function timecheck() {
+	OPTS=$1
+	/usr/sbin/ntpdate $OPTS $SERVER1
 	RETVAL=$?
 	if [ $RETVAL -ne 0 ]; then
-		/usr/sbin/ntpdate $OPTS $SERVER3
+		/usr/sbin/ntpdate $OPTS $SERVER2
 		RETVAL=$?
 		if [ $RETVAL -ne 0 ]; then
-			/usr/sbin/ntpdate $OPTS $SERVER4
+			/usr/sbin/ntpdate $OPTS $SERVER3
 			RETVAL=$?
 			if [ $RETVAL -ne 0 ]; then
-				/usr/sbin/ntpdate $OPTS $SERVER5
+				/usr/sbin/ntpdate $OPTS $SERVER4
+				RETVAL=$?
+				if [ $RETVAL -ne 0 ]; then
+					/usr/sbin/ntpdate $OPTS $SERVER5
+					RETVAL=$?
+				fi
 			fi
 		fi
 	fi
-fi
+	return $RETVAL
+}
+
+# First try server-pool using an unpriviledged port
+timecheck "-s -u"
+RETVAL=$?
+# If the unsecure ports failed, try with using priviledged port
+if [ $RETVAL -ne 0 ]; then timecheck "-s"; fi
