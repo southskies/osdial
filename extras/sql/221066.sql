6dfcfbbc5b32fd95f99b0b1b7001c723b3e40eef
Lott Caskey <lottcaskey@gmail.com>
2010-07-29 05:19:56 +0000

    Add defaults for existing usergroups.

--- extras/sql/221066.sql
+++ extras/sql/221066.sql
@@ -4,8 +4,14 @@
 ALTER TABLE osdial_user_groups ADD allowed_scripts text default ' -ALL-SCRIPTS- -';##|##
  ##    Updates to osdial_user_groups for allowed_scripts.;
 
+UPDATE osdial_user_groups SET allowed_scripts=' -ALL-SCRIPTS- -';##|##
+ ##      Add default values.;
+
 ALTER TABLE osdial_user_groups ADD allowed_email_templates text default ' -ALL-EMAIL-TEMPLATES- -';##|##
  ##    Updates to osdial_user_groups for allowed_email_templates.;
+
+UPDATE osdial_user_groups SET allowed_email_templates=' -ALL-EMAIL-TEMPLATES- -';##|##
+ ##      Add default values.;
 
 CREATE TABLE osdial_email_templates (
   et_id varchar(20) NOT NULL DEFAULT '',
