f75d5b4b87129d2e56a4a9a453b481fdaca36c74
Lott Caskey <lottcaskey@gmail.com>
2010-12-14 06:38:34 +0000

    Set defaults, use @osdial for voicemail.

--- extras/sql/229080.sql
+++ extras/sql/229080.sql
@@ -6,5 +6,8 @@ ALTER TABLE phones ADD voicemail_password VARCHAR(50) DEFAULT '1234';##|##
 ALTER TABLE phones ADD voicemail_email VARCHAR(255) DEFAULT '';##|##
  ##    Adds field for the voicemail email.;
 
+UPDATE phones SET voicemail_password='1234' WHERE voicemail_id!='';##|##
+ ##    Add default passwords.;
+
 UPDATE system_settings SET version='2.2.9.080',last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
  ##    Updating database to version 2.2.9.080 and clearing last_update_check flag.;
