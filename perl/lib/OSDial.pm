2ffd95b7b9cc6ee7ca2cac3c7b7075af868e30f4
Lott Caskey <lottcaskey@gmail.com>
2011-02-04 05:43:22 +0000

    Update of tts-hashing and cache file creation.

--- perl/lib/OSDial.pm
+++ perl/lib/OSDial.pm
@@ -373,50 +373,54 @@ sub agi_output {
 sub agi_tts_sayphrase {
 	my ($self,$phrase,$voice) = @_;
 	if (defined $self->{_agi}) {
-		$voice = 'voice_nitech_us_rms_arctic_hts' unless ($voice);
-		my $hash = md5_hex($voice.':'.$phrase);
-		my $cachedir = "/opt/osdial/tts";
-		my $sdir1 = "/mnt/ramdisk/sounds";
-		my $sdir2 = "/var/lib/asterisk/sounds";
-		my $base = 'tts-'.$hash; 
+		my $ttsfile = $self->tts_generate($phrase,$voice);
+		$self->AGI->stream_file($ttsfile) if ($ttsfile);
+	}
+}
 
-		if (! -f $cachedir.'/'.$base.'.wav') {
-			open(TXT, '>'.$cachedir.'/'.$base.'.txt');
-			print TXT $phrase;
-			close(TXT);
-			my $cmd = '/usr/bin/text2wave '.$cachedir.'/'.$base.'.txt -F 8000 -o '.$cachedir.'/'.$base.'.wav -eval "(' . $voice . ')"';
-			$self->debug(1,'agi_tts_sayphrase','Running %s.',$cmd);
-			system($cmd);
-			unlink($cachedir.'/'.$base.'.txt');
-		} else {
-			$self->debug(1,'agi_tts_sayphrase','%s/%s.wav already exists.',$cachedir,$base);
-		}
+sub tts_generate {
+	my ($self,$phrase,$voice) = @_;
+	$voice = 'voice_nitech_us_rms_arctic_hts' unless ($voice);
+	my $hash = md5_hex($voice.':'.$phrase);
+	my $cachedir = "/opt/osdial/tts";
+	my $sdir1 = "/mnt/ramdisk/sounds";
+	my $sdir2 = "/var/lib/asterisk/sounds";
+	my $base = 'tts-'.$hash; 
 
-		if(-f $cachedir.'/'.$base.'.wav') {
-			if (-d $sdir1) {
-				if (! -d $sdir1.'/tts') {
-					$self->debug(1,'agi_tts_sayphrase','Making directory %s/tts.',$sdir1);
-					mkdir($sdir1.'/tts',oct('0777'));
-				}
-				if (! -f $sdir1.'/tts/'.$base.'.wav') {
-					$self->debug(1,'agi_tts_sayphrase','Copying %s/%s.wav to %s/tts.',$cachedir,$base,$sdir1);
-					system('/bin/cp -au '.$cachedir.'/'.$base.'.wav '.$sdir1.'/tts') unless (-f $sdir1.'/tts/'.$base.'.wav');
-				}
+	if (! -f $cachedir.'/'.$base.'.wav') {
+		open(TXT, '>'.$cachedir.'/'.$base.'.txt');
+		print TXT $phrase;
+		close(TXT);
+		my $cmd = '/usr/bin/text2wave '.$cachedir.'/'.$base.'.txt -F 8000 -o '.$cachedir.'/'.$base.'.wav -eval "(' . $voice . ')"';
+		$self->debug(1,'tts_generate','Running %s.',$cmd);
+		system($cmd);
+		unlink($cachedir.'/'.$base.'.txt');
+	} else {
+		$self->debug(1,'tts_generate','%s/%s.wav already exists.',$cachedir,$base);
+	}
+
+	if(-f $cachedir.'/'.$base.'.wav') {
+		if (-d $sdir1) {
+			if (! -d $sdir1.'/tts') {
+				$self->debug(1,'tts_generate','Making directory %s/tts.',$sdir1);
+				mkdir($sdir1.'/tts',oct('0777'));
 			}
-			if (-d $sdir2) {
-				if (! -d $sdir2.'/tts') {
-					$self->debug(1,'agi_tts_sayphrase','Making directory %s/tts.',$sdir2);
-					mkdir($sdir2.'/tts',oct('0777'));
-				}
-				if (! -f $sdir2.'/tts/'.$base.'.wav') {
-					$self->debug(1,'agi_tts_sayphrase','Copying %s/%s.wav to %s/tts.',$cachedir,$base,$sdir2);
-					system('/bin/cp -au '.$cachedir.'/'.$base.'.wav '.$sdir2.'/tts') unless (-f $sdir2.'/tts/'.$base.'.wav');
-				}
+			if (! -f $sdir1.'/tts/'.$base.'.wav') {
+				$self->debug(1,'tts_generate','Copying %s/%s.wav to %s/tts.',$cachedir,$base,$sdir1);
+				system('/bin/cp -au '.$cachedir.'/'.$base.'.wav '.$sdir1.'/tts') unless (-f $sdir1.'/tts/'.$base.'.wav');
 			}
-
-			$self->debug(1,'agi_tts_sayphrase','Playing tts/%s.',$base);
-			$self->AGI->stream_file('tts/'.$base);
 		}
+		if (-d $sdir2) {
+			if (! -d $sdir2.'/tts') {
+				$self->debug(1,'tts_generate','Making directory %s/tts.',$sdir2);
+				mkdir($sdir2.'/tts',oct('0777'));
+			}
+			if (! -f $sdir2.'/tts/'.$base.'.wav') {
+				$self->debug(1,'tts_generate','Copying %s/%s.wav to %s/tts.',$cachedir,$base,$sdir2);
+				system('/bin/cp -au '.$cachedir.'/'.$base.'.wav '.$sdir2.'/tts') unless (-f $sdir2.'/tts/'.$base.'.wav');
+			}
+		}
+		return 'tts/'.$base;
 	}
 }
 
