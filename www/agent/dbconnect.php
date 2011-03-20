cc4f55784bae5f55b459924f3885cd58141c2504
Lott Caskey <lottcaskey@gmail.com>
2011-03-20 15:11:33 +0000

    Fix formatting.

--- www/agent/dbconnect.php
+++ www/agent/dbconnect.php
@@ -23,44 +23,46 @@
 $conffile = "/etc/osdial.conf";
 $cp2 = explode('/', getcwd());
 $conffile2 = '/' . $cp2[1] . '/' . $cp2[2] . '/db.conf';
-if ( file_exists($conffile2) ) {
-    $conffile = $conffile2;
-}
-if ( file_exists($conffile) )
-{
-$DBCagc = file($conffile);
-foreach ($DBCagc as $DBCline) 
-	{
-	$DBCline = preg_replace("/ |>|\n|\r|\t|\#.*|;.*/","",$DBCline);
-	if (preg_match("/^PATHlogs/", $DBCline))
-		{$PATHlogs = $DBCline;   $PATHlogs = preg_replace("/.*=/","",$PATHlogs);}
-	if (preg_match("/^PATHweb/", $DBCline))
-		{$WeBServeRRooT = $DBCline;   $WeBServeRRooT = preg_replace("/.*=/","",$WeBServeRRooT);}
-	if (preg_match("/^VARDB_server/", $DBCline))
-		{$VARDB_server = $DBCline;   $VARDB_server = preg_replace("/.*=/","",$VARDB_server);}
-	if (preg_match("/^VARDB_database/", $DBCline))
-		{$VARDB_database = $DBCline;   $VARDB_database = preg_replace("/.*=/","",$VARDB_database);}
-	if (preg_match("/^VARDB_user/", $DBCline))
-		{$VARDB_user = $DBCline;   $VARDB_user = preg_replace("/.*=/","",$VARDB_user);}
-	if (preg_match("/^VARDB_pass/", $DBCline))
-		{$VARDB_pass = $DBCline;   $VARDB_pass = preg_replace("/.*=/","",$VARDB_pass);}
-	if (preg_match("/^VARDB_port/", $DBCline))
-		{$VARDB_port = $DBCline;   $VARDB_port = preg_replace("/.*=/","",$VARDB_port);}
-	}
+if (file_exists($conffile2)) $conffile = $conffile2;
 
-}
-else
-{
-#defaults for DB connection
-$VARDB_server = 'localhost';
-$VARDB_user = 'osdial';
-$VARDB_pass = 'osdial1234';
-$VARDB_database = 'osdial';
-$WeBServeRRooT = '/opt/osdial/html';
+if (file_exists($conffile)) {
+    $DBCagc = file($conffile);
+    foreach ($DBCagc as $DBCline) {
+        $DBCline = preg_replace("/ |>|\n|\r|\t|\#.*|;.*/","",$DBCline);
+        if (preg_match("/^PATHlogs/", $DBCline)) {
+            $PATHlogs = $DBCline;
+            $PATHlogs = preg_replace("/.*=/","",$PATHlogs);
+        } elseif (preg_match("/^PATHweb/", $DBCline)) {
+            $WeBServeRRooT = $DBCline;
+            $WeBServeRRooT = preg_replace("/.*=/","",$WeBServeRRooT);
+        } elseif (preg_match("/^VARDB_server/", $DBCline)) {
+            $VARDB_server = $DBCline;
+            $VARDB_server = preg_replace("/.*=/","",$VARDB_server);
+        } elseif (preg_match("/^VARDB_database/", $DBCline)) {
+            $VARDB_database = $DBCline;
+            $VARDB_database = preg_replace("/.*=/","",$VARDB_database);
+        } elseif (preg_match("/^VARDB_user/", $DBCline)) {
+            $VARDB_user = $DBCline;
+            $VARDB_user = preg_replace("/.*=/","",$VARDB_user);
+        } elseif (preg_match("/^VARDB_pass/", $DBCline)) {
+            $VARDB_pass = $DBCline;
+            $VARDB_pass = preg_replace("/.*=/","",$VARDB_pass);
+        } elseif (preg_match("/^VARDB_port/", $DBCline)) {
+            $VARDB_port = $DBCline;
+            $VARDB_port = preg_replace("/.*=/","",$VARDB_port);
+        }
+    }
+} else {
+    #defaults for DB connection
+    $VARDB_server = 'localhost';
+    $VARDB_user = 'osdial';
+    $VARDB_pass = 'osdial1234';
+    $VARDB_database = 'osdial';
+    $WeBServeRRooT = '/opt/osdial/html';
 }
 
-$link=mysql_connect("$VARDB_server", "$VARDB_user", "$VARDB_pass");
-mysql_select_db("$VARDB_database",$link);
+$link=mysql_connect($VARDB_server, $VARDB_user, $VARDB_pass);
+mysql_select_db($VARDB_database,$link);
 
 $local_DEF = 'Local/';
 $conf_silent_prefix = '7';
@@ -68,7 +70,7 @@ $local_AMP = '@';
 $ext_context = 'osdial';
 $recording_exten = '8309';
 $WeBRooTWritablE = '1';
-$non_latin = '0';	# set to 1 for UTF rules, overridden by system_settings
+$non_latin = '0';
 $flag_channels=0;
 $flag_string = 'OSDIALast20';
 
