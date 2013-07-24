82fd1fb4b02119b6d9b42ff72874048ab1810f51
Lott Caskey <lottcaskey@gmail.com>
2013-07-24 18:02:58 -0400

    Missing 301103 db file.

---
+++ extras/sql/301103.sql
@@ -0,0 +1,7 @@
+# 07/24/2013
+
+ALTER TABLE system_settings ADD COLUMN default_ext_context varchar(20) DEFAULT 'osdialEXT';##|##
+ ## Default ext_context for multi-tenant.;
+
+UPDATE system_settings SET version='3.0.1.103',last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
+ ##    Updating database to version 3.0.1.103 and clearing last_update_check flag.;
