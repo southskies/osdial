13e0260db2d10c848f6b2dec70d09a0dde990f20
Lott Caskey <lottcaskey@gmail.com>
2014-03-01 04:17:58 -0500

    Only change 'ALL' to '-ALL-' in additional forms.

--- extras/sql/301122.sql
+++ extras/sql/301122.sql
@@ -1,6 +1,6 @@
 # 02/28/2014
 
-UPDATE osdial_campaign_forms SET campaigns='-ALL-' WHERE campaigns IN ('','ALL');##|##
+UPDATE osdial_campaign_forms SET campaigns='-ALL-' WHERE campaigns IN ('ALL');##|##
  ##Fix non-displayed additional forms.;
 
 UPDATE system_settings SET version='3.0.1.122',last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
