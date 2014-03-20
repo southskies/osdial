c21818bc7a1cb1517ce6a7228c32d707dfed348b
Lott Caskey <lottcaskey@gmail.com>
2014-03-20 11:28:25 -0400

    Bump to version 3.0.2.123.

---
+++ extras/sql/302123.sql
@@ -0,0 +1,4 @@
+# 03/20/2014
+
+UPDATE system_settings SET version='3.0.2.123',last_update_check=DATE_SUB(NOW(), INTERVAL 1 DAY);##|##
+ ##    Updating database to version 3.0.2.123 and clearing last_update_check flag.;
