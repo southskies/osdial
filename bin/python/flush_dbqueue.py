9b4f621dc389aa8fed387540aa318b6463cfff15
Lott Caskey <lottcaskey@gmail.com>
2014-07-02 14:46:21 -0400

    OSDIAL-44 Several example replacement Python scripts.

---
+++ bin/python/flush_dbqueue.py
@@ -0,0 +1,118 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014  Lott Caskey  <lottcaskey@gmail.com>
+#
+
+import sys, os, re, time, pprint, gc
+import pystrix, threading, argparse
+
+import MySQLdb, logging, pystrix
+
+from osdial import OSDial
+
+PROGNAME = 'osdial_flush_dbqueue'
+VERSION = '0.1'
+
+opt = {'verbose':False,'loglevel':False,'debug':False,'test':False,'daemon':False}
+logger = None
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='osdial_keepalive - keeps required subsystems running.')
+    parser.add_argument('-v', '--verbose', action='count', default=0, help='Increases verbosity.', dest='verbose')
+    parser.add_argument('--version', action='version', version='%(prog)s %(ver)s' % {'prog':PROGNAME,'ver':VERSION})
+    parser.add_argument('--debug', action='store_true', help='Run in debug mode.',dest='debug')
+    parser.add_argument('-t', '--test', action='store_true', help='Run in test mode.',dest='test')
+    parser.add_argument('-d', '--daemon', action='store_true', help='Puts process in daemon mode.',dest='daemon')
+    parser.add_argument('-l', '--logLevel', action='store', default='ERROR', choices=['CRITICAL','ERROR','WARNING','INFO','DEBUG'], help='Sets the level of output verbosity.', dest='loglevel')
+    opts = parser.parse_args(args=argv)
+    newargs = vars(opts)
+    for arg in newargs:
+        opt[arg] = newargs[arg]
+
+    osdspt = None
+    try:
+        osdspt = OSDial()
+        FORMAT = '%(asctime)s|%(filename)s:%(lineno)d|%(levelname)s|%(message)s'
+        logger = logging.getLogger()
+        logdeflvl = logging.ERROR
+        if opt.has_key('loglevel') and not opt['loglevel'] is None:
+            logstr2err={'CRITICAL':logging.CRITICAL,'ERROR':logging.ERROR,'WARNING':logging.WARNING,'INFO':logging.INFO,'DEBUG':logging.DEBUG}
+            logdeflvl = logstr2err[opt['loglevel']]
+        logger.setLevel(logdeflvl)
+
+        handler = logging.FileHandler('%s/maintenance.%s' % (osdspt.PATHlogs, time.strftime('%Y-%m-%d', time.gmtime())) )
+        handler.setLevel(logdeflvl)
+        formatter = logging.Formatter(FORMAT)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        if opt['verbose']:
+            handler = logging.StreamHandler()
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter(FORMAT)
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+        
+
+        sptres = osdspt.server_process_tracker(PROGNAME, osdspt.VARserver_ip, os.getpid(), True)
+        osdspt.close()
+        osdspt = None
+        if sptres is True:
+            logger.error("Error process already running!")
+            sys.exit(1)
+    except MySQLdb.OperationalError, e:
+        logger.error("Could not connect to MySQL! %s", e)
+        sys.exit(1)
+    gc.collect()
+
+    logger.info("Starting flushdbqueue_process()")
+    flushdbqueue_process(logger)
+
+
+def flushdbqueue_process(logger):
+    """
+    The routine responsible for flushing the DB queues.
+    """
+    osdial = OSDial()
+    six_hour = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() - 21600))
+    three_hour = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() - 10800))
+    quarter_hour = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() - 900))
+    eigth_hour = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime(time.time() - 450))
+
+    svrlog = {}
+    osdial.sql().execute("SELECT server_ip,vd_server_logs FROM servers WHERE active='Y';")
+    for row in osdial.sql().fetchall():
+        svrlog[row['server_ip']] = row['vd_server_logs']
+
+    flush_time = eigth_hour
+    if re.search('Y',svrlog[osdial.VARserver_ip]):
+        flush_time = quarter_hour
+
+    for svr in svrlog.keys():
+        logger.info(" - Clearing expired manager entries")
+        osdial.sql().execute("DELETE FROM osdial_manager WHERE (server_ip=%s AND ((status='DEAD' AND entry_date<%s) OR entry_date<%s)) OR entry_date<%s;", (svr, flush_time, three_hour, six_hour))
+
+    optimizes = ['osdial_manager','osdial_live_calls','osdial_auto_calls','osdial_hopper']
+    for opttab in optimizes:
+        logger.info(" - Optimizing Table "+opttab)
+        osdial.sql().execute("OPTIMIZE TABLE "+opttab+";")
+        time.sleep(2)
+
+    osdial.close()
+    osdial = None
+    sys.exit(0)
+
+
+def user_main(args):
+    errcode = main(args)
+    return errcode
+
+if __name__ == '__main__':
+    sys.path.insert(0, '%s/python' % os.path.dirname(os.path.realpath(__file__)))
+    try:
+        libname = re.sub('(^osdial_|\..*$)','',os.path.basename(__file__))
+        thislib = __import__(libname)
+        thislib.user_main(sys.argv[1:])
+    except KeyboardInterrupt, e:
+        print >> sys.stderr, "\n\nExiting on user cancel."
+        sys.exit(1)
---
+++ bin/python/manager_kill_hung_congested.py
@@ -0,0 +1,114 @@
+#!/usr/bin/python
+#
+# Copyright (C) 2014  Lott Caskey  <lottcaskey@gmail.com>
+#
+
+import sys, os, re, time, pprint, gc
+import pystrix, threading, argparse
+
+import MySQLdb, logging, pystrix
+
+from osdial import OSDial
+
+PROGNAME = 'osdial_manager_kill_hung_congested'
+VERSION = '0.1'
+
+opt = {'verbose':False,'loglevel':False,'debug':False,'test':False,'daemon':False}
+logger = None
+
+def main(argv):
+    parser = argparse.ArgumentParser(description='osdial_keepalive - keeps required subsystems running.')
+    parser.add_argument('-v', '--verbose', action='count', default=0, help='Increases verbosity.', dest='verbose')
+    parser.add_argument('--version', action='version', version='%(prog)s %(ver)s' % {'prog':PROGNAME,'ver':VERSION})
+    parser.add_argument('--debug', action='store_true', help='Run in debug mode.',dest='debug')
+    parser.add_argument('-t', '--test', action='store_true', help='Run in test mode.',dest='test')
+    parser.add_argument('-d', '--daemon', action='store_true', help='Puts process in daemon mode.',dest='daemon')
+    parser.add_argument('-l', '--logLevel', action='store', default='ERROR', choices=['CRITICAL','ERROR','WARNING','INFO','DEBUG'], help='Sets the level of output verbosity.', dest='loglevel')
+    opts = parser.parse_args(args=argv)
+    newargs = vars(opts)
+    for arg in newargs:
+        opt[arg] = newargs[arg]
+
+    osdspt = None
+    try:
+        osdspt = OSDial()
+        FORMAT = '%(asctime)s|%(filename)s:%(lineno)d|%(levelname)s|%(message)s'
+        logger = logging.getLogger()
+        logdeflvl = logging.ERROR
+        if opt.has_key('loglevel') and not opt['loglevel'] is None:
+            logstr2err={'CRITICAL':logging.CRITICAL,'ERROR':logging.ERROR,'WARNING':logging.WARNING,'INFO':logging.INFO,'DEBUG':logging.DEBUG}
+            logdeflvl = logstr2err[opt['loglevel']]
+        logger.setLevel(logdeflvl)
+
+        handler = logging.FileHandler('%s/maintenance.%s' % (osdspt.PATHlogs, time.strftime('%Y-%m-%d', time.gmtime())) )
+        handler.setLevel(logdeflvl)
+        formatter = logging.Formatter(FORMAT)
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+        if opt['verbose']:
+            handler = logging.StreamHandler()
+            handler.setLevel(logging.INFO)
+            formatter = logging.Formatter(FORMAT)
+            handler.setFormatter(formatter)
+            logger.addHandler(handler)
+        
+
+        sptres = osdspt.server_process_tracker(PROGNAME, osdspt.VARserver_ip, os.getpid(), True)
+        osdspt.close()
+        osdspt = None
+        if sptres is True:
+            logger.error("Error process already running!")
+            sys.exit(1)
+    except MySQLdb.OperationalError, e:
+        logger.error("Could not connect to MySQL! %s", e)
+        sys.exit(1)
+    gc.collect()
+
+    logger.info("Starting killcongest_process()")
+    killcongest_process(logger)
+
+
+def killcongest_process(logger):
+    """
+    Looks for and clears CONGESTED live_sip_channels,
+    4 loops, 1 every 15sec, runs once a minute via cron..
+    """
+    osdial = OSDial()
+
+    for repeat in range(4):
+        CIDdate = time.strftime('%y%m%d%H%M%S', time.gmtime())
+        nowdate = time.strftime('%Y-%m-%d %H:%M:%S', time.gmtime())
+        logger.info(" - Searching for congested channels")
+        osdial.sql().execute("SELECT SQL_NO_CACHE channel FROM live_sip_channels WHERE server_ip=%s AND extension='CONGEST' AND channel LIKE %s LIMIT 100;", (osdial.VARserver_ip, 'Local%'))
+        cong_channels = []
+        lsccnt = osdial.sql().rowcount
+        for row in osdial.sql().fetchall():
+            cong_channels.append(row['channel'])
+        ccnt = 0
+        for channel in cong_channels:
+            KCqueryCID = "KC%02d%s" % (ccnt, CIDdate)
+            osdial.sql().execute("INSERT INTO osdial_manager SET entry_date=%s,status='NEW',response='N',server_ip=%s,action='Hangup',callerid=%s,cmd_line_b=%s;", (nowdate,osdial.VARserver_ip,KCqueryCID,"Channel: %s" % channel))
+            logger.info(" - Killing congested channel "+channel)
+            ccnt += 1
+        if repeat < 3:
+            time.sleep(15)
+
+    osdial.close()
+    osdial = None
+    sys.exit(0)
+
+
+def user_main(args):
+    errcode = main(args)
+    return errcode
+
+if __name__ == '__main__':
+    sys.path.insert(0, '%s/python' % os.path.dirname(os.path.realpath(__file__)))
+    try:
+        libname = re.sub('(^osdial_|\..*$)','',os.path.basename(__file__))
+        thislib = __import__(libname)
+        thislib.user_main(sys.argv[1:])
+    except KeyboardInterrupt, e:
+        print >> sys.stderr, "\n\nExiting on user cancel."
+        sys.exit(1)
