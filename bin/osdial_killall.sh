c9295dcde2f193762552403c90d04c5c21c70361
Lott Caskey <lottcaskey@gmail.com>
2014-07-08 21:10:21 -0400

    Added processes that should be killed.

--- bin/osdial_killall.sh
+++ bin/osdial_killall.sh
@@ -2,4 +2,5 @@
 
 [ -n "`ps -ef | grep FastAGI`" ] && kill -9 `ps -ef | grep FastAGI | awk '{ print $2 }'` > /dev/null 2>&1
 [ -n "`ps -ef | grep AST`" ] && kill -9 `ps -ef | grep AST | awk '{ print $2 }'` > /dev/null 2>&1
-[ -n "`ps -ef | grep OSD`" ] && kill -9 `ps -ef | grep AST | awk '{ print $2 }'` > /dev/null 2>&1
+[ -n "`ps -ef | grep OSD`" ] && kill -9 `ps -ef | grep OSD | awk '{ print $2 }'` > /dev/null 2>&1
+[ -n "`ps -ef | grep '/usr/bin/python /opt/osdial/bin/osdial_'`" ] && kill -9 `ps -ef | grep '/usr/bin/python /opt/osdial/bin/osdial_' | awk '{ print $2 }'` > /dev/null 2>&1
